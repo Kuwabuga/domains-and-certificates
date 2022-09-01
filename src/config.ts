baa4a7086dae97befecb2440e0dab3ab2f99ee7f
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-01 20:11:16 +0100

    Big updates

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,19 @@
 export const DEFAULTS = {
-    comment: "Managed by Terraform CDK",
-    tags: {
-        "default": "default"
-    }
+	comment: "Managed by Terraform CDK",
+	tags: undefined
 }
 
 export const AWS = {
-    region: process.env.AWS_REGION || "us-east-1"
+	region: process.env.AWS_REGION || "us-east-1"
 }
 
 export const BACKEND = {
-    bucket: process.env.AWS_TERRAFORM_BUCKET,
-    key: `${process.env.SERVICE_NAME}/state.tf`,
-    region: AWS.region,
-    acl: "bucket-owner-full-control"
+	bucket: process.env.AWS_TERRAFORM_BUCKET,
+	key: `${process.env.SERVICE_NAME}/state.tf`,
+	region: AWS.region,
+	acl: "bucket-owner-full-control"
 }
 
 export const DOMAINS = [
-    "kuwabuga.com"
+	"kuwabuga.com"
 ]
