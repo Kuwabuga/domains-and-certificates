eb357d1d576caf3e5ee484c0e02744993306f11f
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-02 02:56:52 +0100

    Setup linting rules and pipeline to run on every commit (#9)

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -2,8 +2,8 @@ import { Construct } from "constructs";
 import { AwsProvider } from "@cdktf/provider-aws";
 
 export const buildAWSProvider = (scope: Construct, id: string, region: string) => {
-	return new AwsProvider(scope, `${id}-aws-provider`, {
-		region: region,
-		alias: region
-	});
-}
+  return new AwsProvider(scope, `${id}-aws-provider`, {
+    region: region,
+    alias: region
+  });
+};
